17ee65c0d3e0239ba37a40b7e065161d1ba50a89
Max MARIE <max.marie@finalcad.com>
2021-03-11 11:47:21 +0100

    edit and delete days

--- src/views/Diary.vue
+++ src/views/Diary.vue
@@ -1,19 +1,12 @@
 <template>
-<div class="w-full bg-gray-600 flex-1 flex">
+<div class="bg-gray-600 flex-1 flex">
     <side-menu/>
 
     <!-- Diary -->
-    <div v-if="data && data.days" class="w-full p-4">
+    <div v-if="data && data.days" class="flex-1 p-4">
         <div v-for="day in data.days"
-             :key="day.id"
-             class="rounded bg-gray-500 text-gray-100 w-full p-4">
-             <div class="flex justify-between text-sm">
-                <div class="font-bold">{{day.date}}</div>
-                <div class="flex">
-                    <div class="mr-2 cursor-pointer">DEL</div>
-                    <div class="cursor-pointer">EDIT</div>
-                </div>
-             </div>
+             :key="day.id">
+             <day-box :day="day" :diaryId="data.id"/>
         </div>
     </div>
 
@@ -21,7 +14,8 @@
     <div v-if="errorMsg"
          class="rounded bg-red-500 text-gray-300 p-2 absolute text-xs cursor-pointer"
          style="bottom: 20px; right: 20px;"
-         @click="errorMsg = null"> {{errorMsg}} </div>
+         @click="errorMsg = null"
+         @error="handleError"> {{errorMsg}} </div>
 
 </div>
 </template>
@@ -34,10 +28,12 @@ import store from "../store/store";
 import { ref, onMounted } from "vue"
 
 import SideMenu from "../components/SideMenu"
+import DayBox from "../components/DayBox"
 
 export default {
     components: {
-        SideMenu
+        SideMenu,
+        DayBox
     },
 
     setup() {
@@ -60,13 +56,19 @@ export default {
                 });
         }
 
+        const handleError = (e) => {
+            console.log("error received:", e)
+            errorMsg.value = parsers.error(e)
+        }
+
         // Hooks
         onMounted(fetchDiary)
 
         return {
             data,
             errorMsg,
-            fetchDiary
+            fetchDiary,
+            handleError
         }
 
     }
